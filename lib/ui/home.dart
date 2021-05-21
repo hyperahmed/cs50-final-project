5c3833330f11e702de8585ed37b786edf510ee3f
*ahmeddardiri* <*ahmeddard00@gmail.com*>
2021-05-21 23:25:34 +0200

    hello world

---
+++ lib/ui/home.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+import 'todo_screen.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo App'),
+        backgroundColor: Colors.blue,
+      ),
+      body: TodoScreen(),
+    );
+  }
+}
